a42650f120ce40b49f9bcc1e35fe900bae34d7b1
renegadespork <jessekilburn@gmail.com>
2024-03-26 15:11:28 -0700

    Updated main to calculate fruit inventory.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main() {
-    int x = 33;
-    int y = x+1;
-    printf("%d",y);
-    
+    printf("Taking fruit inventory:\n");
+    int apples = 33;
+    int bananas = apples+9;
+    printf("There are %d banannas.", bananas);
+    double grapes = 58;
+    printf("\nThere are %.3lf grapes. We get precise with grapes.\n", grapes);
+    float price = (apples*0.53)+(bananas*0.44)+(grapes*0.02);
+    printf("------\n Total Price: $%.2f", price);
+    //Just to make it look prettier in the terminal.
     printf("\n");
+    
     return 0;
 }

290a482d5c1567ff1fc6f4fe07490896f949e0ae
renegadespork <jessekilburn@gmail.com>
2024-03-26 15:33:15 -0700

    Added data-types.c and interactive orange inventory.

---
+++ data-types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Go over main variable types.
+    int interger = 4;
+    double dubs = 8e12;
+    float floaty = 14.653;
+    char character = 'x';
+    printf("Int: %d\nDouble: %lf\nFloat: %.3f\nChar: %c\n", interger, dubs, floaty, character);
+    
+    return 0;
+}
--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 int main() {
     printf("Taking fruit inventory:\n");
     int apples = 33;
+    printf("There are %d apples.\n", apples);
     int bananas = apples+9;
     printf("There are %d banannas.", bananas);
     double grapes = 58;
     printf("\nThere are %.3lf grapes. We get precise with grapes.\n", grapes);
-    float price = (apples*0.53)+(bananas*0.44)+(grapes*0.02);
+    int oranges;
+    printf("Please enter the number of oranges:\n");
+    scanf("%d", &oranges);
+    float price = (apples*0.53)+(bananas*0.44)+(grapes*0.02)+(oranges*0.48);
     printf("------\n Total Price: $%.2f", price);
     //Just to make it look prettier in the terminal.
     printf("\n");

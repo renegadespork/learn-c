71f8155ce4ae064ce630728d4b502e06dcd43f20
renegadespork <jessekilburn@gmail.com>
2024-03-27 11:48:27 -0700

    Added some conditional responses to orange values.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 int main() {
     printf("Taking fruit inventory:\n");
     int apples = 33;
@@ -12,14 +14,39 @@ int main() {
     printf("There are %.3lf grapes. We get precise with grapes. Grapes cost $%.2f each.\n", grapes, grapePrice);
     int oranges;
     float orangePrice = 0.48;
-    printf("Please enter the number of oranges. Oranges cost $%.2f each:\n", orangePrice);
+
+    //Now for the interactive portion!
+    printf("Please enter the number of oranges. DO NOT say more than 20. Oranges cost $%.2f each:\n", orangePrice);
     scanf("%d", &oranges);
-    oranges--;
-    printf("I ate an orange while you counted. There are now %d oranges.\n", oranges);
+    if (oranges > 20 && oranges < 100) {
+        printf("User cannot follow instructions. Ending program...\n");
+        sleep(2);
+        printf("Goodbye.\n");
+        return 1;
+    }
+    else if (oranges > 100) {
+        printf("Don't be ridiculous. There aren't that many. Ending program...\n");
+        sleep(2);
+        printf("Goodbye.\n");
+        return 1;
+    }
+    else if (oranges == 0) {
+        printf("No oranges? Okay... :(\n");
+    }
+    else if (oranges < 0) {
+        printf("How can there be negative oranges? Clearly you are incompetent. Ending program...\n");
+        sleep(2);
+        printf("Goodbye.\n");
+        return 1;
+    }
+    else {
+        oranges--;
+        printf("I ate an orange while you counted. There are now %d oranges.\n", oranges);
+    }
     float price = (apples*0.53)+(bananas*0.44)+(grapes*0.02)+(oranges*0.48);
     printf("------\n Total Price: $%.2f", price);
+
     //Just to make it look prettier in the terminal.
     printf("\n");
-    
     return 0;
 }

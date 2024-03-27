6035db6bcc248ad278cdc47c6e612f518ec23257
renegadespork <jessekilburn@gmail.com>
2024-03-27 10:20:52 -0700

    Added data-type conversion. Ignored compiled binaries.

--- data-types.c
+++ data-types.c
@@ -7,6 +7,8 @@ int main() {
     float floaty = 14.653;
     char character = 'x';
     printf("Int: %d\nDouble: %lf\nFloat: %.3f\nChar: %c\n", interger, dubs, floaty, character);
-    
+    //Type conversions
+    double floatyTrunc = (int)floaty;
+    printf("Truncated Float: %.1lf\n", floatyTrunc);
     return 0;
 }
